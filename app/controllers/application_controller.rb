43918bdcec9f4e1ab3d6ab04024746a43d2c3ca8
ryutazero2 <ryutazero2@yahoo.co.jp>
2020-08-05 20:53:41 +0900

    ユーザー機能指摘事項やバリデート関連など

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
+    rescue_from Exception, with: :logged_in_user
     include SessionsHelper
 
     private
     # ログイン済みユーザーかどうか確認
-    def logged_in_user
-        unless logged_in?
-        redirect_to login_url
+    def logged_in_user(e)
+        @exception = e
+        redirect_to login_url unless logged_in?
+    end
+
+    # ログイン状態を確認する
+    def forbid_login_user
+        if @current_user 
+            flash[:notice] = 'すでにログインしています'
+            redirect_to root_path
         end
     end
+    
 end
