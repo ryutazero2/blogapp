c3f055916f9e638f0109c7cb17597ac786069e6e
ryutazero2 <ryutazero2@yahoo.co.jp>
2020-09-22 23:44:43 +0900

    bootstrap対応

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: %i[new]
 
   def post_params
-    params.require(:post).permit(:title,:content)
+    params.require(:post).permit(:title, :content)
   end
 
   # rubocop:disable Metrics/AbcSize:
